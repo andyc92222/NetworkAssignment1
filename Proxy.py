bcee199aed7826bfdd65d73e364fb6ab1525cae1
andyc92222 <126429483+andyc92222@users.noreply.github.com>
2025-03-29 06:05:03 +1030

    4
    Making the proxy to work when a cached file is requested

--- Proxy.py
+++ Proxy.py
@@ -118,6 +118,8 @@ while True:
     # ProxyServer finds a cache hit
     # Send back response to client 
     # ~~~~ INSERT CODE ~~~~
+    for line in cacheData:
+    clientSocket.send(line.encode())
     # ~~~~ END CODE INSERT ~~~~
     cacheFile.close()
     print ('Sent to the client:')
